e2199ec1a6a78c501aa177d02e9d966e85214728
Monika Dogra <monikaa8dec@gmail.com>
2021-07-01 11:09:14 +0530

    Text_blob_dictionary

---
+++ app.py
@@ -0,0 +1,30 @@
+from logging import debug
+from flask import Flask,render_template,request
+from textblob import Word
+
+# craete a flask app
+app = Flask(__name__)
+
+# define an api
+@app.route('/')
+def welcome():
+    return render_template('base.html') 
+
+@app.route("/explain", methods = ["POST"])
+def explain():
+    # get the info:
+    word = request.form.get('Word')
+
+    # take the word to expalin and give its definition
+    output = Word(word).definitions
+
+    # check :
+    if not output:
+        return render_template('base.html',Explaination = f"Please enter a valid word")
+    else:
+        return render_template('base.html',Explaination = f"{word} means : {output}")
+
+
+if __name__ == '__main__':
+    
+    app.run()
